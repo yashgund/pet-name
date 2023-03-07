fa5d075c94f4828d2e2c66c555ca78012b385af1
Yash <78315880+yashgund@users.noreply.github.com>
2023-03-07 18:22:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react'
 
 function App() {
+  const petDetails = [
+   { name: 'Buddy',
+    age: 2},
+    {
+      name: 'bruno',
+      age: 3
+    },
+    {
+      name: 'tuffy',
+      age: 1
+    }
+]
+const [pet, setPet] = useState(petDetails)
+useEffect(() => {
+  return () => {
+    pet
+  };
+}, []);
+
+function sortArr(pet){
+  pet.sort((a, b)=>(
+    a.age > b.age ? 1 : -1
+  ));
+  setPet(pet)
+  console.log(petDetails);
+  console.log('hi');
+}
+
+// sortArr()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <table>
+      <th>
+        <tr key="">
+        <td>name</td>
+        <td>age</td>
+        </tr>
+      </th>
+      <tbody>
+        {petDetails && petDetails.map((ele)=>(
+          <tr key={ele.age}>
+          <td>{ele.name}</td>
+          <td>{ele.age}</td>
+          </tr>
+        ))}  
+      </tbody>
+     </table>
+     <button onClick = {()=>(
+      sortArr(petDetails)
+  )}>sort</button>
     </div>
   );
 }
